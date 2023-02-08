e427f0067689462fb9e59e77cce10b39b4a78d90
ronelm2000 <ronelm2000@gmail.com>
2023-02-08 22:33:23 +0800

    Adds new package
    Adds the wsmtools export template in the installer package

--- weiss2.mse-game/devtools/build.ps1
+++ weiss2.mse-game/devtools/build.ps1
@@ -1,4 +1,4 @@
-.\..\..\..\magicseteditor.exe --create-installer weiss2.mse-game weiss2-real-multiple-layouts.mse-style weiss2-symbols.mse-symbol-font weiss2-symbols-sp.mse-symbol-font
+.\..\..\..\magicseteditor.exe --create-installer weiss2.mse-game weiss2-real-multiple-layouts.mse-style weiss2-symbols.mse-symbol-font weiss2-symbols-sp.mse-symbol-font weiss2-wsmtools.mse-export-template
 $compress = @{
 Path= "..\..\weiss2-real-multiple-layouts.mse-style\font\*"
 CompressionLevel = "Fastest"
